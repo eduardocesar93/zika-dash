faeeb13a0cb52c19122e107f1d9fd01f16e23513
João Marcos Gris <joaomarcosgris@hotmail.com>
2016-12-03 10:38:56 -0200

    Add zika por bairro normalizado

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,14 +1,30 @@
 # -*- coding: utf-8 -*-
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+import pandas as pd
+
 
 UPLOAD_FOLDER = 'static/images/'
 app = Flask(__name__)
-
+# data
+tweets = pd.read_csv('../data/tweets-dataset-limpo.csv')
+casos = pd.read_csv('../data/casos-por-bairro-limpo.csv')
+clima = pd.read_csv('../data/dados-climaticos-limpo.csv')
+casos_zika_por_bairro = casos[casos['doenca'] == 'Zika']['bairro'].value_counts()
+populacao_por_bairro = casos[['bairro', 'populacao']].drop_duplicates()
+populacao_por_bairro = pd.Series(populacao_por_bairro['populacao'].tolist(),index=populacao_por_bairro['bairro'].tolist())
+casos_zika_por_bairro_normalizado = casos_zika_por_bairro / populacao_por_bairro
+casos_zika_por_bairro_normalizado = casos_zika_por_bairro_normalizado.sort_values().dropna().drop_duplicates().drop('BRAS DE PINA')
 
 @app.route("/")
 def index():
     return render_template('index.html')
+
+
+@app.route('/zika-por-bairro')
+def zika_por_bairro():
+    return jsonify(dict(casos_zika_por_bairro_normalizado))
+
 
 if __name__ == "__main__":
     app.secret_key = 'Ajuda_o_Maluco_Que_Tá_Doente! Birl! Saí_De_Casa_e_Comi_Para_#&!@!#$!!!'
