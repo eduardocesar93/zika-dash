a6e5b3e2d47d4ae5b7219eee4f562691330da36d
João Marcos Gris <joaomarcosgris@hotmail.com>
2016-12-03 13:17:13 -0200

    Add dengue e zika

--- dashboard/app.py
+++ dashboard/app.py
@@ -10,11 +10,21 @@ app = Flask(__name__)
 tweets = pd.read_csv('../data/tweets-dataset-limpo.csv')
 casos = pd.read_csv('../data/casos-por-bairro-limpo.csv')
 clima = pd.read_csv('../data/dados-climaticos-limpo.csv')
+# zika
 casos_zika_por_bairro = casos[casos['doenca'] == 'Zika']['bairro'].value_counts()
 populacao_por_bairro = casos[['bairro', 'populacao']].drop_duplicates()
 populacao_por_bairro = pd.Series(populacao_por_bairro['populacao'].tolist(),index=populacao_por_bairro['bairro'].tolist())
 casos_zika_por_bairro_normalizado = casos_zika_por_bairro / populacao_por_bairro
 casos_zika_por_bairro_normalizado = casos_zika_por_bairro_normalizado.sort_values().dropna().drop_duplicates().drop('BRAS DE PINA')
+# dengue
+casos_dengue_por_bairro = casos[casos['doenca'] == 'Dengue']['bairro'].value_counts()
+casos_dengue_por_bairro_normalizado = casos_dengue_por_bairro / populacao_por_bairro
+casos_dengue_por_bairro_normalizado = casos_dengue_por_bairro_normalizado.sort_values().dropna().drop_duplicates().drop('BRAS DE PINA')
+# chikungunya
+casos_chik_por_bairro = casos[casos['doenca'] == 'Chikungunya']['bairro'].value_counts()
+casos_chik_por_bairro_normalizado = casos_chik_por_bairro / populacao_por_bairro
+casos_chik_por_bairro_normalizado = casos_chik_por_bairro_normalizado.sort_values().dropna().drop_duplicates().drop('BRAS DE PINA')
+
 
 @app.route("/")
 def index():
@@ -24,6 +34,15 @@ def index():
 @app.route('/zika-por-bairro')
 def zika_por_bairro():
     return jsonify(dict(casos_zika_por_bairro_normalizado))
+
+
+@app.route('/dengue-por-bairro')
+def dengue_por_bairro():
+    return jsonify(dict(casos_dengue_por_bairro_normalizado))
+
+@app.route('/chikungunya-por-bairro')
+def chik_por_bairro():
+    return jsonify(dict(casos_chik_por_bairro_normalizado))
 
 
 if __name__ == "__main__":
